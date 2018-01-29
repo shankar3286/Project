1da9fbf6cdcf0e2ef2b892be95c302a5dbf7f2e8
Shankargouda <skarigouda87@gmail.com>
2018-01-29 16:01:11 +0000

    testing

---
+++ src/devops/integration/LibraryDAOImpl.java
@@ -0,0 +1,60 @@
+package devops.integration;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import devops.business.Library;
+
+public class LibraryDAOImpl implements LibraryDAO {
+	private final Logger logger ;
+	private SessionFactory sessionFactory ;
+	private Session session ;
+	
+	public LibraryDAOImpl() {
+		logger = LoggerFactory.getLogger( getClass() ) ;
+		try {
+			logger.info( "Opening connection" ); ;
+			sessionFactory = new Configuration().configure( "hibernate.cfg.xml" ).buildSessionFactory() ;
+			session = sessionFactory.openSession() ;
+		} catch ( HibernateException e ) {
+			logger.error( "Hibernate Exception", e ) ;
+			System.exit( 1 ) ;
+		}
+	}
+	@Override
+	public void close() {
+		logger.info( "Closing connection" ) ;
+		sessionFactory.close() ;
+	}
+	@Override
+	public Integer save( Library library) {
+		session.beginTransaction() ;
+		Integer id = (Integer) session.save( library ) ;
+		session.getTransaction().commit() ;
+		return id ;
+	}
+	@Override
+	public void delete( Library library ) {
+		session.beginTransaction() ;
+		session.delete( library  ) ;
+		session.getTransaction().commit() ;
+	}
+	@Override
+	public List<Library> findAll() {
+		return session.createQuery( "From Library", Library.class ).getResultList() ;
+	}
+
+	@Override
+	public List<Library> getDockerData() {
+		
+		return session.createQuery( "From Library", Library.class ).getResultList() ;
+	}
+
+	
+}
